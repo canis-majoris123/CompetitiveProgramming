e5e9b6ec1fc42acf380235a73b6ece8c6d620923
canis-majoris123 <48183997+canis-majoris123@users.noreply.github.com>
2020-06-09 13:36:25 +0530

    Create MyTemplate.cpp

---
+++ HackerEarth/MyTemplate.cpp
@@ -0,0 +1,22 @@
+// In the name of Aadi Shakti
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+#define int long long
+#define fast std::ios_base::sync_with_stdio(false); cin.tie(0) ; cout.tie(0) ;
+
+void solve()
+{
+
+}
+
+int32_t main()
+{
+    int testcase  = 1 ;
+    while(testcase--)
+    {
+        solve();
+    }
+    return 0 ;
+}
